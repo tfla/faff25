7d4e27586f64a2d0aeece7f0488f24e390a2db8b
tfla <timmy.larsson@bodhilinux.com>
2015-02-06 21:05:59 +0100

    Finished arguments.

--- model.py
+++ model.py
@@ -4,24 +4,29 @@
 A simple single-layer climate model.
 """
 
+__author__ = "Timmy Larsson, Linus Lexfors, Hans Bjerndell"
+__license__ = "GPLv2"
+
 import argparse
 
-def Tj(fs = 1370, a = 0.28, sigma = 5.67E-8, f = 0.77):
-	return (fs*(1-a)/(4*sigma*(1-(f/2))))**(1/4)
+def Tj():
+    return (args.fs*(1-args.a)/(4*args.sigma*(1-(args.f/2))))**(1/4)
 
 def main():
-	"""
+    """
     Main function for running the model.
     Options will be parsed from the command line.
     """
-	print("%d" % (Tj()))
+    global args 
+    args = parse_args()
+    print("%d" % (Tj()))
 
 def parse_args():
     parser = argparse.ArgumentParser(description='A single-layer climate model.')
-    parser.add_argument('-fs', type=float, default=1370, help='Sun constant')
-    parser.add_argument('-a', type=float, default=0.28, help='Albedo')
-    parser.add_argument('-sigma', type=float, default=6.67E-8, help='Stefan-Boltzmanns constant')
-    parser.add_argument('-f', type=float, default=0.77, help='Absorption capacity of the atmosphere')
+    parser.add_argument('-fs', type=float, nargs='?', default=1370, help='Sun constant')
+    parser.add_argument('-a', type=float, nargs='?', default=0.28, help='Albedo')
+    parser.add_argument('-sigma', type=float, nargs='?', default=5.67E-8, help='Stefan-Boltzmanns constant')
+    parser.add_argument('-f', type=float, nargs='?', default=0.77, help='Absorption capacity of the atmosphere')
 
     return parser.parse_args()
 
